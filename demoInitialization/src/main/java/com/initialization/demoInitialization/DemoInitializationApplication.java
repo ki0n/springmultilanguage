8f60ed39f61a700c597ec4b10867abdf631721fb
Kion <daniarkano@gmail.com>
2018-09-22 14:02:42 +0200

    Spring multi language example

---
+++ demoInitialization/src/main/java/com/initialization/demoInitialization/DemoInitializationApplication.java
@@ -0,0 +1,12 @@
+package com.initialization.demoInitialization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoInitializationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoInitializationApplication.class, args);
+	}
+}
